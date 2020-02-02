f652dcdc858083bb84e13f660bd9a3012282ee27
linai <linaicode@gmail.com>
2020-02-02 17:03:57 +0800

    feat(java): java版第一题

--- src/title1/Solution.java
+++ src/title1/Solution.java
@@ -1,4 +1,4 @@
-class Solution {
+public class Solution {
   public int[] twoSum(final int[] nums, final int target) {
      for (int i = 0; i < nums.length; i++) {
           for (int j = i + 1; j < nums.length; j++) {
